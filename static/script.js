13dfa65ffa9b81a5127ec26b9d80dc7f49525b01
Judith2007 <84209053+Judith2007@users.noreply.github.com>
2023-07-05 00:25:00 -0600

    Update script.js

--- static/script.js
+++ static/script.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
                 $('#sentiment').text(prediction)
                 $('#sentiment').show()
 
-                $('#emoji').attr('src', emoji_url);
+                $('#emoji').attr('src', emoji_url)
                 $('#emoji').show()
             },
 
